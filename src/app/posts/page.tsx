d76df5f6f594d7d423c26c421fad914d99ddba0c
caueeugenio <caue_eugenio@hotmail.com>
2024-09-21 17:11:54 -0300

    adjustment into post list design

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import {
   SearchBar,
@@ -6,12 +6,12 @@ import {
   Divider,
   PageContainer,
   withAuth,
-} from '@/components';
-import { usePostsContext } from '@/contexts';
-import { ROLES } from '@/enums/role';
-import { formatDate, formatTime } from '@/utils/dateAndTime';
-import { useEffect } from 'react';
-
+  Button,
+} from "@/components";
+import { usePostsContext } from "@/contexts";
+import { ROLES } from "@/enums/role";
+import { formatDate, formatTime } from "@/utils/dateAndTime";
+import { useEffect } from "react";
 const Posts = () => {
   const { posts, loadPosts } = usePostsContext();
 
@@ -22,18 +22,32 @@ const Posts = () => {
 
   return (
     <PageContainer>
-      <PageTitle title='Lista de Posts' />
+      <PageTitle title="Lista de Posts" />
       <Divider />
-      <SearchBar className='block md:hidden' />
-
+      <SearchBar className="block md:hidden" />
       {posts.map((post) => {
         return (
-          <div key={post.id} className='flex flex-col mb-8'>
-            <h2>{post.title}</h2>
-            <p>{post.content}</p>
-            <span>{post.author}</span>
-            <span>{formatDate(post.date)}</span>
-            <span>{formatTime(post.date)}</span>
+          <div
+            key={post.id}
+            className="flex flex-col mb-8 
+          "
+          >
+            <h2 className="text-primary font-bold">{post.title}</h2>
+            <span className="font-normal text-[15px] leading-[18.15px]">
+              {formatDate(post.date)} - Por {post.author} - às{" "}
+              {formatTime(post.date)}
+            </span>
+            <div className="flex flex-row-reverse ">
+              <img
+                src="https://media.istockphoto.com/id/173015527/photo/a-single-red-book-on-a-white-surface.jpg?s=612x612&w=0&k=20&c=AeKmdZvg2_bRY2Yct7odWhZXav8CgDtLMc_5_pjSItY="
+                alt="imagem default" className="w-32 h-32 mr-4 border border-primary rounded-md " /> <div> <p>{post.content}</p> </div>
+            </div>
+            <Button
+              className="bg-primary text-white h-12 w-1/3 p-2 rounded-md mt-4"
+              type="submit"
+            >
+              Continuar a leitura...
+            </Button>
           </div>
         );
       })}
